bd2a41241808c5043c1efc7727a9b7458e8c80bb
Mayon Maël <mael.mayon@free.fr>
2017-03-02 23:23:08 +0100

    Fix header ?

--- head.php
+++ head.php
@@ -17,27 +17,27 @@
 <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700italic,900' rel='stylesheet' type='text/css'>
 
 <!-- Sass -->
-<link href="stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
-<link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
-<link href="/postSass.css" rel="stylesheet" type="text/css" />
+<link href="/public/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<link href="/pulbic/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+<link href="/public/postSass.css" rel="stylesheet" type="text/css" />
 <!--[if IE]>
   <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
 <![endif]-->
 
 <!-- Prism -->
-<link href="/stylesheets/prism.css" rel="stylesheet" type="text/css" />
-<script src="/js/prism.js"></script>
+<link href="/public/stylesheets/prism.css" rel="stylesheet" type="text/css" />
+<script src="/public/js/prism.js"></script>
 
 <!-- imgSlider -->
-<link href="/stylesheets/imgslider-2.0.1-min.css" rel="stylesheet" type="text/css" />
-<script src="/js/imgslider-2.0.1-min.js"></script>
+<link href="/public/stylesheets/imgslider-2.0.1-min.css" rel="stylesheet" type="text/css" />
+<script src="/public/js/imgslider-2.0.1-min.js"></script>
 
 <!-- Moment.js -->
 <script src="http://momentjs.com/downloads/moment.js"/></script>
 <script src="http://momentjs.com/downloads/moment-with-locales.js"/></script>
 
 <!-- SweetAlert -->
-<script src="/js/sweetalert.min.js"></script>
+<script src="/public/js/sweetalert.min.js"></script>
 <link rel="stylesheet" type="text/css" href="/js/sweetalert.css">
 
 <script type="text/javascript">
